20c9826f2dd10024cfc7b63e27ae840173801d32
morfeli <fmoreira95@hotmail.com>
2023-01-28 20:33:42 -0500

    working on adding content now

--- components/Sections/About.tsx
+++ components/Sections/About.tsx
@@ -4,7 +4,7 @@ export const About = () => {
   return (
     <SectionLayout link="About">
       <h2 className="text-2xl">About</h2>
-      <p className="py-6 text-lg leading-8">
+      <p className="py-6 text-lg leading-8 lg:w-[750px] lg:text-center lg:mx-auto">
         I'm a Frontend Engineer based in sunny Miami, FL. I build pixel perfect
         and robust architect web apps. Passionate in providing a phenomenal user
         experience, clean code, and working within a team of likeminded
