276d4f43049895a40d7ec595670b55d98823973a
morfeli <fmoreira95@hotmail.com>
2023-02-08 08:17:33 -0500

    made alot of changes

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -10,25 +10,25 @@ const quicksand400 = Quicksand({ subsets: ["latin"], weight: "400" });
 const quicksand500 = Quicksand({ subsets: ["latin"], weight: "500" });
 
 export default function Home() {
-  useEffect(() => {
-    let section = document.querySelectorAll("section");
+  // useEffect(() => {
+  //   let section = document.querySelectorAll("section");
 
-    window.onscroll = () => {
-      section.forEach((sec) => {
-        let top = window.scrollY + 150;
-        let offset = sec.offsetTop;
-        let height = sec.offsetHeight;
-        let id = sec.getAttribute("id");
-        const target = document.getElementById(`navLink-${id}`);
+  //   window.onscroll = () => {
+  //     section.forEach((sec) => {
+  //       let top = window.scrollY + 150;
+  //       let offset = sec.offsetTop;
+  //       let height = sec.offsetHeight;
+  //       let id = sec.getAttribute("id");
+  //       const target = document.getElementById(`navLink-${id}`);
 
-        if (top >= offset && top < offset + height) {
-          target!.style.backgroundColor = "#6c757d";
-        } else {
-          target!.style.backgroundColor = "";
-        }
-      });
-    };
-  }, []);
+  //       if (top >= offset && top < offset + height) {
+  //         target!.style.backgroundColor = "#6c757d";
+  //       } else {
+  //         target!.style.backgroundColor = "";
+  //       }
+  //     });
+  //   };
+  // }, []);
 
   return (
     <>
