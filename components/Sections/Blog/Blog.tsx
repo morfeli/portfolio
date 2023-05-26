1ff40022b0211b649dc2cf3a76d704919fdf1ddc
morfeli <fmoreira95@hotmail.com>
2023-05-25 20:08:25 -0400

    new: added 1st blog post element

--- components/Sections/Blog/Blog.tsx
+++ components/Sections/Blog/Blog.tsx
@@ -3,7 +3,14 @@ import { SectionLayout } from "../Layouts/SectionLayout";
 export const Blog = () => {
   return (
     <SectionLayout link="Blog">
-      <p className="py-24 italic">Coming soon...</p>
+      <div className="w-[400px] bg-gray-400 rounded-md p-4 my-4 mx-auto shadow-xl">
+        <h1 className="text-xl">Redis... the in-memory database.</h1>
+        <p className="py-2">
+          I break down the concepts into clear concise chunks to get you started
+          in building a high performace web application.
+        </p>
+        <p className="italic">Coming soon!</p>
+      </div>
     </SectionLayout>
   );
 };
