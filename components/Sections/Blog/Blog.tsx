a2952085942fba08ce10b1b498de0180fa3a6cd6
morfeli <fmoreira95@hotmail.com>
2023-05-31 23:00:13 -0400

    new: added pyro to project list

--- components/Sections/Blog/Blog.tsx
+++ components/Sections/Blog/Blog.tsx
@@ -3,11 +3,11 @@ import { SectionLayout } from "../Layouts/SectionLayout";
 export const Blog = () => {
   return (
     <SectionLayout link="Blog">
-      <div className="w-[400px] bg-gray-400 rounded-md p-4 my-4 mx-auto shadow-xl">
-        <h1 className="text-xl">Redis... the in-memory database.</h1>
-        <p className="py-2">
-          I break down the concepts into clear concise chunks to get you started
-          in building a high performace web application.
+      <div className="p-4 mx-auto my-4 bg-gray-400 rounded-md shadow-xl sm:w-[350px]">
+        <h1 className="text-lg">Redis... the in-memory database.</h1>
+        <p className="py-2 text-sm">
+          I break down the concepts into clear & concise chunks to get you
+          started in building a high performace web application.
         </p>
         <p className="italic">Coming soon!</p>
       </div>
