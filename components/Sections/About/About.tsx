9fcaac380482182e2b88edd062d3ab900b595a98
morfeli <fmoreira95@hotmail.com>
2023-09-10 21:32:10 -0400

    fix: about and new blog post

--- components/Sections/About/About.tsx
+++ components/Sections/About/About.tsx
@@ -4,15 +4,10 @@ export const About = () => {
   return (
     <SectionLayout link="About">
       <p className="py-6 text-lg  text-center leading-8 xl:w-[750px] lg:mx-auto md:px-20">
-        I am a Frontend Engineer located in Miami, FL. I'm driven by my passion
-        to deliver exceptional user experiences and write clean code. I thrive
-        in collaborative environments where I can work alongside like-minded
-        individuals who share the same dedication and vision.
-      </p>
-
-      <p className="text-sm text-left w-[290px] mx-auto">
-        “Do not jump to any conclusions about your own capabilities. You are
-        fine - just keep at.” -Elouqent Javascript by Marijn Haverbeke
+        I'm a Software Developer located in Miami, FL. I thrive in collaborative
+        environments where I can work alongside like-minded individuals who
+        share the same dedication and passion. I love building complex user
+        interfaces and learning new skills on a consistent basis.
       </p>
     </SectionLayout>
   );
--- components/Sections/Blog/Blog.tsx
+++ components/Sections/Blog/Blog.tsx
@@ -3,16 +3,27 @@ import { SectionLayout } from "../Layouts/SectionLayout";
 export const Blog = () => {
   return (
     <SectionLayout link="Blog">
-      <div className="p-4 mx-auto my-4 bg-gray-400 rounded-md shadow-xl sm:w-[350px]">
-        <a href="https://medium.com/@morfelidev/speed-durable-and-high-performant-its-redis-5d503711a6f5">
-          <h1 className="text-lg underline">
-            Speed, Durable and High Performant. It's Redis!
-          </h1>
-        </a>
-        <p className="py-2 text-sm">
-          A warm intro on Redis and its elegant features.
-        </p>
-        <p className="italic">July 10, 2023</p>
+      <div className="flex justify-evenly">
+        <div className="p-4  my-4 bg-gray-400 rounded-md shadow-xl sm:w-[350px]">
+          <a href="https://medium.com/@morfelidev/speed-durable-and-high-performant-its-redis-5d503711a6f5">
+            <h2 className="text-lg underline">
+              Speed, Durable and High Performant. It's Redis!
+            </h2>
+          </a>
+          <p className="py-2 text-sm">
+            A warm intro on Redis and its elegant features.
+          </p>
+          <p className="italic">July 10, 2023</p>
+        </div>
+        <div className="p-4  my-4 bg-gray-400 rounded-md shadow-xl sm:w-[350px]">
+          <a href="https://medium.com/@morfelidev/the-art-of-polyglot-coding-267b413d50ae">
+            <h2 className="text-lg underline">The Art of Polygot Coding</h2>
+          </a>
+          <p className="py-2 text-sm">
+            Explore beyond from what you know, discovering a new journey.
+          </p>
+          <p className="italic">July 10, 2023</p>
+        </div>
       </div>
     </SectionLayout>
   );
