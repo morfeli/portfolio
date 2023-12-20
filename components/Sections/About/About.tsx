259f63872b1fcdfd56cfe84f2234cf376e88c977
morfeli <fmoreira95@hotmail.com>
2023-12-20 09:55:34 -0500

    fix: about me

--- components/Sections/About/About.tsx
+++ components/Sections/About/About.tsx
@@ -5,13 +5,12 @@ export const About = () => {
     <SectionLayout link="About">
       <p className="py-4 text-lg  text-center leading-8 xl:w-[750px] lg:mx-auto md:px-20">
         I'm a Software Developer based in Miami, FL. I thrive in collaborative
-        environments where I can work alongside like-minded individuals who
-        share the same dedication and passion in building renowed software. I'm
-        driven by an immeasurable passion for delivering high-end solutions to
-        complex architecture design objectives. I continuously learn new skills
-        to keep myself at the forefront. Working with technology brings me a
-        sense of satisfaction as my work comes to life and positively impacts
-        it's users.
+        environments where I enjoy working alongside like-minded individuals.
+        I'm driven by an immeasurable passion for delivering high-end solutions
+        that solves complex architecture design objectives. I continuously learn
+        new skills to keep myself at the forefront. Working with technology
+        brings me a sense of satisfaction as my work comes to life while
+        positively impacting it's users.
       </p>
     </SectionLayout>
   );
