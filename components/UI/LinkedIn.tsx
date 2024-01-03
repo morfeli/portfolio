5a3c9e7c7fdd15a7c855e8eebd770b32aa1b3b5a
morfeli <fmoreira95@hotmail.com>
2024-01-02 20:57:00 -0500

    fix: link

--- components/UI/LinkedIn.tsx
+++ components/UI/LinkedIn.tsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 const LinkedIn = () => {
   return (
-    <Link href="https://www.linkedin.com/in/morfelidev/" target="_blank">
+    <Link href="https://www.linkedin.com/in/felipelmoreira/" target="_blank">
       <div className="flex items-center justify-center w-12 h-12 transition-all rounded-full cursor-pointer hover:bg-[#6c757d]">
         <svg
           xmlns="http://www.w3.org/2000/svg"
